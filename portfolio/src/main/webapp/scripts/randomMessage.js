1c82ca9f4680cc5a8a6448203314c5bc562f0109
jamari-morrison <RedCater360@yahoo.com>
2021-06-11 15:53:42 +0000

    Incomplete troubleshooting for js button

--- portfolio/src/main/webapp/scripts/randomMessage.js
+++ portfolio/src/main/webapp/scripts/randomMessage.js
@@ -1,8 +1,9 @@
+
 async function randomizeMessage() {
     console.log("wokrking")
   const responseFromServer = await fetch('/randomMessage');
   const textFromResponse = await responseFromServer.text();
 
-  const msgContainer = document.getElementById('randomMessageTxt');
+  const msgContainer = document.getElementById('random-message-txt');
   msgContainer.innerText = textFromResponse;
 }
